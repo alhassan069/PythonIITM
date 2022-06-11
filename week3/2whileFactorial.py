ac1255bda2e116d29a5216901819fd3697d1ec63
Alimul Hassan <alhassan069@gmail.com>
2022-06-11 15:29:38 +0530

    wk3L3

---
+++ week3/2whileFactorial.py
@@ -0,0 +1,29 @@
+# Lets find the factorial of a number
+
+# n=5
+# 1*2*3*4*5
+
+# answer = 1
+# answer = answer * 2
+# answer = answer * 3
+# answer = answer * 4
+# answer = answer * 5
+# answer will be 120
+
+# now do it using while loop
+
+print('enter a number: ')
+
+n =int(input())
+
+
+
+i = 1
+answer = 1
+
+while(i<=n):
+    answer = answer*i
+    i=i+1
+
+
+print(answer)
