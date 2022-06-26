46a6b409765b3e96bb4495aafc0e3207be114639
Alimul Hassan <alhassan069@gmail.com>
2022-06-26 17:43:21 +0530

    week 3 10,11

---
+++ week3/10nestedForLoop.py
@@ -0,0 +1,11 @@
+# TWO BROTHERS SHARATH AND TANMAY
+# s = 'VIBGYOR'
+# t = 'VIBGYOR'
+
+# count = 0
+# for i in range(len(s)):
+#     for j in range(7):
+#         # if i != j:
+#         count = count + 1
+#         print(i, j, s[i], s[j])
+# print(count)
